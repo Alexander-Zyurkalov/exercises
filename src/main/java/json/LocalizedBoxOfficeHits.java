eff78fe04af9102d289a8735048a9fdd66ac9565
Alexandr Zyurkalov <alexandr.zyurkalov@raisin.com>
2021-05-16 19:30:29 +0200

    green irritates me

---
+++ src/main/java/json/LocalizedBoxOfficeHits.java
@@ -0,0 +1,30 @@
+package json;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+
+public class LocalizedBoxOfficeHits {
+    public void printMovieDetails(ResourceBundle resBundle) {
+        String movieName = resBundle.getString("MovieName");
+        Long revenue = (Long)(resBundle.getObject("GrossRevenue"));
+        Integer year = (Integer) resBundle.getObject("Year");
+        System.out.println("Movie " + movieName + "(" + year + ")" + " grossed "
+                + revenue );
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        LocalizedBoxOfficeHits localizedHits = new LocalizedBoxOfficeHits();
+        // print the largest box-office hit movie for default (US) locale
+        Locale locale = Locale.getDefault();
+        localizedHits.printMovieDetails(ResourceBundle.getBundle("ResBundle", locale));
+
+        // print the largest box-office hit movie for Italian locale
+        locale = new Locale("it", "IT", "");
+        localizedHits.printMovieDetails(ResourceBundle.getBundle("ResBundle", locale));
+        ResourceBundle.getBundle("ResBundle", locale).
+
+    }
+}
