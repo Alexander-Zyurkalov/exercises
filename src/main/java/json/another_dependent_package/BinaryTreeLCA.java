e552ab6d0a8160ec3e47ed8dfd7d5be7c79d125d
Alexandr Zyurkalov <alexandr.zyurkalov@raisin.com>
2021-05-18 22:13:33 +0200

    :-(

--- src/main/java/json/another_dependent_package/BinaryTreeLCA.java
+++ src/main/java/json/another_dependent_package/BinaryTreeLCA.java
@@ -14,17 +14,12 @@ class TreeNode {
 
 
 public class BinaryTreeLCA {
-    public static TreeNode convertToTreeNode(List<Integer> treeNums, int num1, int num2) {
+    public static TreeNode convertToTreeNode(TreeNode root, List<Integer> treeNums, int num1, int num2) {
         int i = 0;
+        root.left = new TreeNode(treeNums.remove(1));
+        root.right = new TreeNode(treeNums.remove(2));
 
-
-        TreeNode treeNode = new TreeNode(treeNums.remove(0));
-        treeNode.left = new TreeNode(treeNums.remove(1));
-        treeNode.right = new TreeNode(treeNums.remove(2));
-
-
-
-        return treeNode;
+        return root;
     }
     public static String BinaryTreeLCA(String[] strArr) {
         // code goes here
@@ -33,7 +28,7 @@ public class BinaryTreeLCA {
         int num2 = Integer.parseInt(strArr[2]);
 
         List<Integer> treeNums = Arrays.stream(tree.split(", *")).map(Integer::parseInt).collect(Collectors.toList());
-        TreeNode treeNode = convertToTreeNode(treeNums, num1, num2);
+        TreeNode treeNode = convertToTreeNode(new TreeNode(treeNums.remove(0)), treeNums, num1, num2);
 
         return strArr[0];
     }
