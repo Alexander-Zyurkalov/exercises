ab7dde8590250bb989c38b36df328f77dcaf85bd
Alexandr Zyurkalov <alexandr.zyurkalov@raisin.com>
2021-07-07 21:05:03 +0200

    fix the error

--- src/main/java/zyurkalov/NumberOfProvinces.java
+++ src/main/java/zyurkalov/NumberOfProvinces.java
@@ -26,27 +26,27 @@ public class NumberOfProvinces {
     public int findCircleNumBFS(int[][] isConnected) {
         int[] visited = new int[isConnected.length];
         int count = 0;
-        Queue<Integer> queue = new LinkedList<>();
+        Queue < Integer > queue = new LinkedList < > ();
         for (int i = 0; i < isConnected.length; i++) {
             if (visited[i] == 0) {
                 queue.add(i);
-            }
-            while (!queue.isEmpty()) {
-                int s = queue.remove();
-                visited[s] = 1;
-                for (int j = 0; j < isConnected.length; j++) {
-                    if (isConnected[s][j] == 1 && visited[j] == 0)
-                        queue.add(j);
+                while (!queue.isEmpty()) {
+                    int s = queue.remove();
+                    visited[s] = 1;
+                    for (int j = 0; j < isConnected.length; j++) {
+                        if (isConnected[s][j] == 1 && visited[j] == 0)
+                            queue.add(j);
+                    }
                 }
+                count++;
             }
-            count++;
         }
         return count;
     }
 
     public static void main(String[] args) {
         System.out.println(
-                new NumberOfProvinces().findCircleNum(new int[][]{{1,1,0},{1,1,0},{0,0,1}})
+                new NumberOfProvinces().findCircleNumBFS(new int[][]{{1,1,0},{1,1,0},{0,0,1}})
         );
     }
 
