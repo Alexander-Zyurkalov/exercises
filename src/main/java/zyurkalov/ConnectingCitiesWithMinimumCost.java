42b3a282957957eee67e0776712e155a75ecd8bd
Alexandr Zyurkalov <alexandr.zyurkalov@raisin.com>
2021-07-10 21:10:34 +0200

    simple sum is ready

--- src/main/java/zyurkalov/ConnectingCitiesWithMinimumCost.java
+++ src/main/java/zyurkalov/ConnectingCitiesWithMinimumCost.java
@@ -1,8 +1,22 @@
 package zyurkalov;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class ConnectingCitiesWithMinimumCost {
+    private final static int NODE1 = 0;
+    private final static int NODE2 = 1;
+    private final static int WEIGHT = 2;
     public int minimumCost(int n, int[][] connections) {
-        return 3;
+
+        int sum = Arrays.stream(connections)
+                .sorted(Comparator.comparingInt(connection -> connection[WEIGHT]))
+                .peek(connection -> {
+
+                })
+                .mapToInt(connection -> connection[WEIGHT])
+                .sum();
+        return sum;
 
     }
 
