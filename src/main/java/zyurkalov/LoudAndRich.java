5b2818e941823bd437b1b74446cf77ffe6fd2294
Alexandr Zyurkalov <alexandr.zyurkalov@raisin.com>
2021-07-16 21:20:05 +0200

    adds transforming the array

--- src/main/java/zyurkalov/LoudAndRich.java
+++ src/main/java/zyurkalov/LoudAndRich.java
@@ -1,13 +1,38 @@
 package zyurkalov;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class LoudAndRich {
-    public int[] loudAndRich(int[][] richer, int[] quiet) {
-        return new int[]{};
+
+    private static class DFS{
+        private final Map<Integer, List<Integer>> graph;
+        private final int[] quiet;
+
+        public DFS(final Map<Integer, List<Integer>> graph, final int[] quiet) {
+            this.graph = graph;
+            this.quiet = quiet;
+        }
+        public int dfs(int i){
+            return 1;
+        }
     }
 
+    public int[] loudAndRich(int[][] richer, int[] quiet) {
+        Map<Integer, List<Integer>> graph = new HashMap<>(quiet.length);
+        for (int[] ints : richer) {
+            int i = ints[1];
+            graph.putIfAbsent(i, new ArrayList<>());
+            graph.get(i).add(ints[0]);
+        }
+        var dfs = new DFS(graph, quiet);
+        return IntStream.range(0,quiet.length).map(dfs::dfs).toArray();
+    }
+
+
+
     public static void main(String[] args) {
+
         var output = Arrays.toString(new LoudAndRich().loudAndRich(
                 new int[][]{{1,0},{2,1},{3,1},{3,7},{4,3},{5,3},{6,3}},
                 new int[]{3,2,5,4,6,1,7,0}
