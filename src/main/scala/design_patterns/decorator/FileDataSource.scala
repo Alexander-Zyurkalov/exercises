e1e6564b0862f509e0089cd5a66b5861d5af4d5d
Alexandr Zyurkalov <alexandr.zyurkalov@raisin.com>
2021-07-28 21:58:46 +0200

    continue at this state

--- src/main/scala/design_patterns/decorator/FileDataSource.scala
+++ src/main/scala/design_patterns/decorator/FileDataSource.scala
@@ -1,5 +1,9 @@
 package design_patterns.decorator
 
-class FileDataSource {
+class FileDataSource(private val name: String) extends DataSource {
+  override def writeData(data: String): Unit = {
+    val file = File(this.name)
+  }
 
+  override def readData: String = ???
 }
