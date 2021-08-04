cb9160a47c082a3608e8f5bfe89d257853b6e8c8
Alexandr Zyurkalov <alexandr.zyurkalov@raisin.com>
2021-08-04 21:06:55 +0200

    seems working, but no time for propper testing.

--- src/main/java/zyurkalov/challenges/IdealoChallenge1.java
+++ src/main/java/zyurkalov/challenges/IdealoChallenge1.java
@@ -1,10 +1,35 @@
 package zyurkalov.challenges;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class IdealoChallenge1 {
 
+    public int solution(String S) {
+        Map<String, Integer> counter = new HashMap<>();
+        for (int len = 1; len < S.length() + 1; len++) {
+            for (int i = 0; i < S.length() - len + 1; i++) {
+                String substring = S.substring(i, i + len);
+                counter.merge(substring, 1, (oldValue, newValue) -> oldValue + 1);
+            }
 
+            for (String key:counter.keySet()) {
+                if (counter.get(key) == 1)
+                    return key.length();
+            }
+        }
+        return 1;
+
+    }
 
     public static void main(String[] args) {
         IdealoChallenge1 idealoChallenge1 = new IdealoChallenge1();
+        System.out.println(idealoChallenge1.solution("abaaba"));
     }
 }
+/*
+
+abaaba
+
+
+ */
