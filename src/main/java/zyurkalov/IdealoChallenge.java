80fabb97ac44778176c5eb11c71611d2bcd931f4
Alexandr Zyurkalov <alexandr.zyurkalov@raisin.com>
2021-08-04 20:22:50 +0200

    demo test

---
+++ src/main/java/zyurkalov/IdealoChallenge.java
@@ -0,0 +1,19 @@
+package zyurkalov;
+
+import java.util.Arrays;
+
+public class IdealoChallenge {
+
+    public int solution(int[] A) {
+        int[] array = Arrays.stream(A).filter(a -> a > 0).sorted().toArray();
+        for (int i = 0; i < array.length; i++) {
+            if (i == array.length-1 || array[i+1] - array[i] > 1)
+                return array[i] + 1;
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new IdealoChallenge().solution(new int[]{1, 2, 3}));
+    }
+}
