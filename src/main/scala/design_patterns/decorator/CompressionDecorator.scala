90ba751cd72e673a910705cafa60117e35e34eed
Alexandr Zyurkalov <alexandr.zyurkalov@raisin.com>
2021-08-30 20:22:23 +0200

    Fixes the code after downgrading to the old scala

--- src/main/scala/design_patterns/decorator/CompressionDecorator.scala
+++ src/main/scala/design_patterns/decorator/CompressionDecorator.scala
@@ -1,49 +1,49 @@
-//package design_patterns.decorator
-//
-//import java.io.ByteArrayOutputStream
-//
-//case class CompressionDecorator(wrappe: DataSource) extends DataSourceDecorator(wrappe){
-//  override def writeData(data: String): Unit = {
-//    super.writeData(compress(data))
-//  }
-//
-//  override def readData: String = decompress(super.readData)
-//
-//
-//  import java.io.{ByteArrayInputStream, IOException}
-//  import java.util.Base64
-//  import java.util.zip.{Deflater, DeflaterOutputStream, InflaterInputStream}
-//
-//  private val compLevel: Int = 6
-//
-//  private def compress(stringData: String) = {
-//    val data = stringData.getBytes
-//    try {
-//      val bout = new ByteArrayOutputStream(512)
-//      val dos = new DeflaterOutputStream(bout, new Deflater(compLevel))
-//      dos.write(data)
-//      dos.close()
-//      bout.close()
-//      Base64.getEncoder.encodeToString(bout.toByteArray)
-//    }
-//  }
-//
-//  private def decompress(stringData: String) = {
-//    val data = Base64.getDecoder.decode(stringData)
-//    try {
-//      val in = new ByteArrayInputStream(data)
-//      val iin = new InflaterInputStream(in)
-//      val bout = new ByteArrayOutputStream(512)
-//      def readIt: LazyList[Int] = iin.read() #:: readIt
-//      readIt.takeWhile(b => b >= 0).foreach(b => bout.write(b))
-//      in.close()
-//      iin.close()
-//      bout.close()
-//      new String(bout.toByteArray)
-//    } catch {
-//      case ex: IOException =>
-//        null
-//    }
-//  }
-//}
-//
+package design_patterns.decorator
+
+import java.io.ByteArrayOutputStream
+
+case class CompressionDecorator(wrappe: DataSource) extends DataSourceDecorator(wrappe){
+  override def writeData(data: String): Unit = {
+    super.writeData(compress(data))
+  }
+
+  override def readData: String = decompress(super.readData)
+
+
+  import java.io.{ByteArrayInputStream, IOException}
+  import java.util.Base64
+  import java.util.zip.{Deflater, DeflaterOutputStream, InflaterInputStream}
+
+  private val compLevel: Int = 6
+
+  private def compress(stringData: String) = {
+    val data = stringData.getBytes
+    try {
+      val bout = new ByteArrayOutputStream(512)
+      val dos = new DeflaterOutputStream(bout, new Deflater(compLevel))
+      dos.write(data)
+      dos.close()
+      bout.close()
+      Base64.getEncoder.encodeToString(bout.toByteArray)
+    }
+  }
+
+  private def decompress(stringData: String) = {
+    val data = Base64.getDecoder.decode(stringData)
+    try {
+      val in = new ByteArrayInputStream(data)
+      val iin = new InflaterInputStream(in)
+      val bout = new ByteArrayOutputStream(512)
+      def readIt: Stream[Int] = iin.read() #:: readIt
+      readIt.takeWhile(b => b >= 0).foreach(b => bout.write(b))
+      in.close()
+      iin.close()
+      bout.close()
+      new String(bout.toByteArray)
+    } catch {
+      case ex: IOException =>
+        null
+    }
+  }
+}
+
--- src/main/scala/design_patterns/decorator/Main.scala
+++ src/main/scala/design_patterns/decorator/Main.scala
@@ -1,19 +1,19 @@
-//package design_patterns.decorator
-//
-//object Main extends App {
-//  val salaryRecords: String = "Name,Salary\nJohn Smith,100000\nSteven Jobs,912000"
-//  println(salaryRecords)
-//  val encoded:DataSource = CompressionDecorator(
-//    EncryptionDecorator(
-//      FileDataSource("out/OutputDemo.txt")
-//    )
-//  )
-//  encoded.writeData(data = salaryRecords)
-//  val plain: DataSource = new FileDataSource("out/OutputDemo.txt")
-//  println("- Input ----------------")
-//  println(salaryRecords)
-//  println("- Encoded --------------")
-//  println(plain.readData)
-//  println("- Decoded --------------")
-//  println(encoded.readData)
-//}
+package design_patterns.decorator
+
+object Main extends App {
+  val salaryRecords: String = "Name,Salary\nJohn Smith,100000\nSteven Jobs,912000"
+  println(salaryRecords)
+  val encoded:DataSource = CompressionDecorator(
+    EncryptionDecorator(
+      FileDataSource("out/OutputDemo.txt")
+    )
+  )
+  encoded.writeData(data = salaryRecords)
+  val plain: DataSource = new FileDataSource("out/OutputDemo.txt")
+  println("- Input ----------------")
+  println(salaryRecords)
+  println("- Encoded --------------")
+  println(plain.readData)
+  println("- Decoded --------------")
+  println(encoded.readData)
+}
