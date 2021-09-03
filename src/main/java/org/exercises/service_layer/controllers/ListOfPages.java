5a46769ea5c1465890e60575c33077aead4749fb
Alexandr Zyurkalov <alexandr.zyurkalov@raisin.com>
2021-09-03 21:35:30 +0200

    deletes the module

--- src/main/java/org/exercises/service_layer/controllers/ListOfPages.java
+++ src/main/java/org/exercises/service_layer/controllers/ListOfPages.java
@@ -1,14 +1,22 @@
 package org.exercises.service_layer.controllers;
 
+import org.apache.catalina.connector.InputBuffer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
 
 @Controller
 public class ListOfPages {
 
-    @RequestMapping("/")
-    public ModelAndView index() {
-        return new ModelAndView("home");
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public String home() {
+        return "home";
+
+
     }
 }
