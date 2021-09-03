e03b9a66982fbc5fc7ca6c2e403097566f110427
Alexandr Zyurkalov <alexandr.zyurkalov@raisin.com>
2021-09-03 21:40:25 +0200

    deletes all spring parts from the main module and creates a sub module with spring boot

--- application/src/main/java/com/zyurkalov/exercises/application/Application.java
+++ application/src/main/java/com/zyurkalov/exercises/application/Application.java
@@ -1,15 +1,13 @@
-package com.example.caching;
+package com.zyurkalov.exercises.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
-public class CachingApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(CachingApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
