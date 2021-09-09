7eb13f7829346aa065ca26c029a0e1843551e247
Alexandr Zyurkalov <alexandr.zyurkalov@raisin.com>
2021-09-09 22:14:32 +0200

    sets the types of variables accourding to the book The Programmer's Brain

--- src/main/java/zyurkalov/leetcode_solutions/ParallelCoursesBFSSolution.java
+++ src/main/java/zyurkalov/leetcode_solutions/ParallelCoursesBFSSolution.java
@@ -5,43 +5,43 @@ import java.util.List;
 
 public class ParallelCoursesBFSSolution {
     public int minimumSemesters(int N, int[][] relations) {
-        int[] inCount = new int[N + 1]; // or indegree
-        List<List<Integer>> graph = new ArrayList<>(N + 1);
+        int[] gathererInCount = new int[N + 1]; // or indegree
+        List<List<Integer>> organiserGraph = new ArrayList<>(N + 1);
         for (int i = 0; i < N + 1; ++i) {
-            graph.add(new ArrayList<Integer>());
+            organiserGraph.add(new ArrayList<Integer>());
         }
         for (int[] relation : relations) {
-            graph.get(relation[0]).add(relation[1]);
-            inCount[relation[1]]++;
+            organiserGraph.get(relation[0]).add(relation[1]);
+            gathererInCount[relation[1]]++;
         }
-        int step = 0;
-        int studiedCount = 0;
-        List<Integer> bfsQueue = new ArrayList<>();
+        int stepperAndMostWanterHolderStep = 0;
+        int stepperStudiedCount = 0;
+        List<Integer> containerBfsQueue = new ArrayList<>();
         for (int node = 1; node < N + 1; node++) {
-            if (inCount[node] == 0) {
-                bfsQueue.add(node);
+            if (gathererInCount[node] == 0) {
+                containerBfsQueue.add(node);
             }
         }
         // start learning with BFS
-        while (!bfsQueue.isEmpty()) {
+        while (!containerBfsQueue.isEmpty()) {
             // start new semester
-            step++;
-            List<Integer> nextQueue = new ArrayList<>();
-            for (int node : bfsQueue) {
-                studiedCount++;
-                for (int endNode : graph.get(node)) {
-                    inCount[endNode]--;
+            stepperAndMostWanterHolderStep++;
+            List<Integer> containerNextQueue = new ArrayList<>();
+            for (int node : containerBfsQueue) {
+                stepperStudiedCount++;
+                for (int endNode : organiserGraph.get(node)) {
+                    gathererInCount[endNode]--;
                     // if all prerequisite courses learned
-                    if (inCount[endNode] == 0) {
-                        nextQueue.add(endNode);
+                    if (gathererInCount[endNode] == 0) {
+                        containerNextQueue.add(endNode);
                     }
                 }
             }
-            bfsQueue = nextQueue;
+            containerBfsQueue = containerNextQueue;
         }
 
         // check if learn all courses
-        return studiedCount == N ? step : -1;
+        return stepperStudiedCount == N ? stepperAndMostWanterHolderStep : -1;
     }
 
     public static void main(String[] args) {
