199b2109d3cbac98488c0e8a41fe8af03213baa4
Alexandr Zyurkalov <alexandr.zyurkalov@raisin.com>
2021-09-13 08:23:35 +0200

    doing the Programmer's brain exercise

--- src/main/java/zyurkalov/leetcode_solutions/ParallelCoursesDFSSolution.java
+++ src/main/java/zyurkalov/leetcode_solutions/ParallelCoursesDFSSolution.java
@@ -5,49 +5,49 @@ import java.util.List;
 
 public class ParallelCoursesDFSSolution {
     public int minimumSemesters(int N, int[][] relations) {
-        List<List<Integer>> graph = new ArrayList<>(N + 1);
+        List<List<Integer>> graph = new ArrayList<>(N + 1); // graph is an organiser
         for (int i = 0; i < N + 1; ++i) {
-            graph.add(new ArrayList<Integer>());
+            graph.add(new ArrayList<>());
         }
-        for (int[] relation : relations) {
+        for (int[] relation : relations) { // relation is a stepper
             graph.get(relation[0]).add(relation[1]);
         }
         // check if the graph contains a cycle
-        int[] visited = new int[N + 1];
-        for (int node = 1; node < N + 1; node++) {
+        int[] visitStatus = new int[N + 1]; // visitStatus is a flag
+        for (int node = 1; node < N + 1; node++) { // node is a stepper
             // if has cycle, return -1
-            if (dfsCheckCycle(node, graph, visited) == -1) {
+            if (dfsCheckCycle(node, graph, visitStatus) == -1) {
                 return -1;
             }
         }
 
         // if no cycle, return the longest path
         int[] visitedLength = new int[N + 1];
-        int maxLength = 1;
-        for (int node = 1; node < N + 1; node++) {
+        int maxLength = 1; // maxLength is a MostWantedHolder
+        for (int node = 1; node < N + 1; node++) { // node is a stepper
             int length = dfsMaxPath(node, graph, visitedLength);
             maxLength = Math.max(length, maxLength);
         }
         return maxLength;
     }
 
-    private int dfsCheckCycle(int node, List<List<Integer>> graph, int[] visited) {
+    private int dfsCheckCycle(int node, List<List<Integer>> graph, int[] visitStatus) {
         // return -1 if has a cycle
         // return 1 if does not have any cycle
-        if (visited[node] != 0) {
-            return visited[node];
+        if (visitStatus[node] != 0) {
+            return visitStatus[node];
         } else {
             // mark as visiting
-            visited[node] = -1;
+            visitStatus[node] = -1;
         }
-        for (int endNode : graph.get(node)) {
-            if (dfsCheckCycle(endNode, graph, visited) == -1) {
+        for (int endNode : graph.get(node)) { // endNode is a stepper
+            if (dfsCheckCycle(endNode, graph, visitStatus) == -1) {
                 // we meet a cycle!
                 return -1;
             }
         }
-        // mark as visited
-        visited[node] = 1;
+        // mark as visitStatus
+        visitStatus[node] = 1;
         return 1;
     }
 
@@ -56,8 +56,8 @@ public class ParallelCoursesDFSSolution {
         if (visitedLength[node] != 0) {
             return visitedLength[node];
         }
-        int maxLength = 1;
-        for (int endNode : graph.get(node)) {
+        int maxLength = 1; // maxLength is the most wanted holder
+        for (int endNode : graph.get(node)) { // endNode is a stepper
             int length = dfsMaxPath(endNode, graph, visitedLength);
             maxLength = Math.max(length + 1, maxLength);
         }
